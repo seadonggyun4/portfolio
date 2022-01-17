5af9f78864679fbdb3e1e4ddbc4792fe899c431c
seadonggyun4 <seadonggyun@gmail.com>
2022-01-17 13:08:14 +0900

    💄 skill 카테고리 filp 애니메이션 dealy 타임 과 tilte 효과 moseover시 적용

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// wave animation ============================================================================================
+
 let wave1 = document.getElementById('wave1');
 let wave2 = document.getElementById('wave2');
 let wave3 = document.getElementById('wave3');
@@ -13,29 +15,32 @@ window.addEventListener('scroll',function(){
   wave4.style.backgroundPositionX = 100 + value + -2 + 'px'
 })
 
-
-var flip = TweenMax.to('.box',0.5,{
-  transform: rotateY(0)
-})
-
-
-
+// flip animation ============================================================================================
+var controller = new ScrollMagic.Controller();
 
 const boxEls = document.querySelectorAll('.box');
+
 boxEls.forEach(function(boxEl){
   new ScrollMagic
       .Scene({
-        triggerElement: boxEl,
-        triggerHook: 1
+        triggerElement: boxEl,// 애니메이션 시작 객체
+        triggerHook: 1, // trigger 위치
+        reverse:false //애니메이션 한번만실행
       })
-      .setTween(boxEl, flip) // setClassToggle(적용될 식별자, 토글될 클래스 명)
-    .addTo(new ScrollMagic.Controller());
+      .setClassToggle(boxEl,'flip') // 클래스 이름 토글
+      .addTo(controller);
+});
+
+
+// flip animation ============================================================================================
+boxEls.forEach(function(boxEl){
+  boxEl.addEventListener("mouseover", function(){
+    VanillaTilt.init(document.querySelectorAll(".box"), {
+      max: 35, //최대 휘어짐
+      speed: 400, // 값이 낮을수록 더 빠르게 변한다.
+    });
+    
+  })
 })
 
-console.log(boxEls.classList);
-
-// VanillaTilt.init(document.querySelectorAll(".box"), {
-//   max: 35, //최대 휘어짐
-//   speed: 400, // 값이 낮을수록 더 빠르게 변한다.
-// });
 
